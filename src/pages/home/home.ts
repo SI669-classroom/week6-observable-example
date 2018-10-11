5871adccb10edf8fce6adc57e978b0330596619b
Chetan Keshav <kchetan@umich.edu>
2018-10-11 14:25:55 -0400

    observable example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	nums: number[] = [];
+	numUpdate: any;
+	numObserver: any;
 
-  }
+	constructor(public navCtrl: NavController) {
+
+		this.numUpdate = Observable.create(observer => {
+			this.numObserver = observer;
+		});
+
+		this.numUpdate.subscribe((num) => {
+			this.nums.push(num)
+		})
+
+		this.watch7(this.numObserver);
+		this.watch3(this.numObserver);
+		this.watch17(this.numObserver);
+		this.watch60(this.numObserver);
+		this.watch600(this.numObserver);
+	}
+
+	watch7(observer) {
+		setInterval(() => {
+			observer.next(7);
+		}, 700)
+	}
+
+	watch3(observer) {
+		setInterval(() => {
+			observer.next(3);
+		}, 300)
+	}
+
+	watch17(observer) {
+		setInterval(() => {
+			observer.next(17);
+		}, 1700)
+	}
+
+	watch60(observer) {
+		setInterval(() => {
+			observer.next(60);
+		}, 6000)
+	}
+
+	watch600(observer) {
+
+		setTimeout(() => {
+			observer.next(180);
+			observer.complete();
+		}, 18000)
+	}
 
 }
